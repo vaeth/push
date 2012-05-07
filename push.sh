8c83ea3eb541d19b1b9ae7b86a9ffa387fa98267
Martin Väth <vaeth@mathematik.uni-wuerzburg.de>
2012-05-07 17:35:14 +0200

    Avoid pollution of global name space as far as possible.

--- push.sh
+++ push.sh
@@ -4,32 +4,33 @@
 
 Push() {
 	case ${1} in
-	-c)	shift
-		eval ${1}=;;
+	-c)	PushA_=
+		shift;;
+	*)	eval PushA_=\${${1}};;
 	esac
 	PushB_=${1}
-	eval PushA_=\${${1}}
 	shift
 	for PushE_
-	do	[ -z "${PushA_:+-}" ] || PushA_="${PushA_} "
-		PushF_=:
+	do	[ -z "${PushA_:++}" ] || PushA_="${PushA_} "
+		unset PushF_
 		case ${PushE_:-=} in
 		[=~]*)	PushF_=false;;
 		esac
 		PushC_=${PushE_}
 		while PushD_=${PushC_%%\'*}
-		do	if ${PushF_} && case ${PushD_} in
+		do	if ${PushF_-:} && case ${PushD_} in
 			*[!-+=~/:.0-9_a-zA-Z]*)	false;;
 			esac
 			then	PushA_=${PushA_}${PushD_}
 			else	PushA_="${PushA_}'${PushD_}'"
-				PushF_=:
+				unset PushF_
 			fi
 			[ "${PushD_}" = "${PushC_}" ] && break
 			PushA_="${PushA_}\\'"
 			PushC_=${PushC_#*\'}
 		done
 	done
-	eval ${PushB_}=\${PushA_}
-	[ -n "${PushA_:+-}" ]
+	eval "${PushB_}=\${PushA_}
+	unset PushA_ PushB_ PushC_ PushD_ PushE_
+	[ -n \"${${PushB_}:++}\" ]"
 }
